578f9ce42931f8907aac7c339458ac066466b268
evanlemuel23 <evanlemuel5@gmail.com>
2025-02-15 18:23:59 +0530

    Final Code

---
+++ DIGITALCLOCK/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DigitalClock from "./DigitalClock"; // Importing the DigitalClock component
+import "./App.css"; // Import styles if needed
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1></h1>
+      <DigitalClock />
+    </div>
+  );
+}
+
+export default App;
